f0a12ade5e514e8176d280646506a43b8d1671e7
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-12-31 16:26:51 +0700

    Update main.py

--- Learn/Flower/main.py
+++ Learn/Flower/main.py
@@ -2,7 +2,7 @@ from screen_driver import ST7789Screen
 
 def main():
     screen = ST7789Screen()
-    path = "./Water_lily_opening_bloom_20fps.ogv"
+    path = "./Water_lily_opening_bloom_20fps.mp4"
     screen.display_video(path, loop=False)
     screen.clear()
 
