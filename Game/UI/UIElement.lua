130739d23845ae4cc17ce99fac38ee7922f7e5ad
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-01-16 23:17:00 +0700

    Create UIElement.lua

---
+++ Game/UI/UIElement.lua
@@ -0,0 +1,15 @@
+local UIElement = {}
+UIElement.__index = UIElement
+
+function UIElement:new(x, y, width, height)
+    local self = setmetatable({}, UIElement)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    return self
+end
+
+function UIElement:draw()
+    error("UIElement:draw() not implemented")
+end
