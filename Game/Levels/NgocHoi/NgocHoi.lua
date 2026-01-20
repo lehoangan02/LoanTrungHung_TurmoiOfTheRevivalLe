fdd80eee14f5b555671eeca747b7e0ac6f2fed19
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-01-20 12:46:38 +0700

    Add bullet and missed bullet animations to NgocHoi level
    Introduced new bullet and missed bullet sprites and animations in the NgocHoi level. Updated the logic to trigger and display these animations at specific times. Added corresponding image assets for bullets and missed bullets.

--- Game/Levels/NgocHoi/NgocHoi.lua
+++ Game/Levels/NgocHoi/NgocHoi.lua
@@ -39,6 +39,7 @@ function NgocHoi:load()
     NgocHoi.soldier_spritesheet:setFilter("nearest", "nearest")
     NgocHoi.soldier_grid = anim8.newGrid(24, 37, NgocHoi.soldier_spritesheet:getWidth(), NgocHoi.soldier_spritesheet:getHeight())
     NgocHoi.soldier_animations = {}
+
     for i = 1, 3 do
         local anim = anim8.newAnimation(
             NgocHoi.soldier_grid('1-12', 1),
@@ -47,6 +48,29 @@ function NgocHoi:load()
         anim:gotoFrame((i - 1) * 4 + 1)
         NgocHoi.soldier_animations[i] = anim
     end
+
+    NgocHoi.bullet_spritesheet = love.graphics.newImage("Resources/Images/Bullet.png")
+    NgocHoi.bullet_spritesheet:setFilter("nearest", "nearest")
+    NgocHoi.bullet_grid = anim8.newGrid(50, 9, NgocHoi.bullet_spritesheet:getWidth(), NgocHoi.bullet_spritesheet:getHeight())
+    NgocHoi.bullet_animation = anim8.newAnimation(NgocHoi.bullet_grid('1-3', 1), 0.1, 'pauseAtEnd')
+    NgocHoi.bulletX = 185
+    NgocHoi.bulletY = 110
+    NgocHoi.TimeBullet1 = 6
+    NgocHoi.Bullet1Fired = false
+    NgocHoi.TimeBullet2 = 7
+    NgocHoi.Bullet2Fired = false
+    NgocHoi.TimeBullet3 = 7.5
+    NgocHoi.Bullet3Fired = false
+    NgocHoi.BulletTimer = 0
+
+    NgocHoi.missed_bullet_spritesheet = love.graphics.newImage("Resources/Images/Missed_bullet.png")
+    NgocHoi.missed_bullet_spritesheet:setFilter("nearest", "nearest")
+    NgocHoi.missed_bullet_grid = anim8.newGrid(240, 3, NgocHoi.missed_bullet_spritesheet:getWidth(), NgocHoi.missed_bullet_spritesheet:getHeight())
+    NgocHoi.missed_bullet_animation = anim8.newAnimation(NgocHoi.missed_bullet_grid('1-3', 1), 0.1, 'pauseAtEnd')
+    NgocHoi.missed_bulletX = 0
+    NgocHoi.missed_bulletY = 120
+    NgocHoi.missed_bullet_fired = false
+    NgocHoi.Time_missed_bullet = 8.5
 
     NgocHoi.groundWidth = NgocHoi.ground:getWidth()
     NgocHoi.groundPositionX = 0
@@ -96,6 +120,9 @@ function NgocHoi:update(dt)
         NgocHoi.soldier_animations[i]:update(dt)
     end
 
+    NgocHoi.bullet_animation:update(dt)
+    NgocHoi.missed_bullet_animation:update(dt)
+
     for i = 1, 3 do
         NgocHoi.strawTimers[i] = NgocHoi.strawTimers[i] + dt
         if NgocHoi.strawTimers[i] >= NgocHoi.strawIntervals[i] then
@@ -116,8 +143,40 @@ function NgocHoi:update(dt)
         NgocHoi.shakeY = 0
     end
 
-    if (InputManager:isEventFKeyPressed()) then
-        NgocHoi:shake(0.05, 1)
+    NgocHoi.BulletTimer = NgocHoi.BulletTimer + dt
+    if NgocHoi.BulletTimer >= NgocHoi.TimeBullet1 and NgocHoi.BulletTimer < NgocHoi.TimeBullet2 then
+        if NgocHoi.Bullet1Fired == false then
+            NgocHoi.Bullet1Fired = true
+            NgocHoi:shake(0.08, 1)
+            NgocHoi.bullet_animation:gotoFrame(1)
+            NgocHoi.bullet_animation:resume()
+        end
+    elseif NgocHoi.BulletTimer >= NgocHoi.TimeBullet2 and NgocHoi.BulletTimer < NgocHoi.TimeBullet3 then
+        if NgocHoi.Bullet2Fired == false then
+            NgocHoi.Bullet2Fired = true
+            NgocHoi:shake(0.08, 1)
+            NgocHoi.bullet_animation:gotoFrame(1)
+            NgocHoi.bullet_animation:resume()
+            NgocHoi.bulletX = 187
+            NgocHoi.bulletY = 170
+        end
+    elseif NgocHoi.BulletTimer >= NgocHoi.TimeBullet3 then
+        if NgocHoi.Bullet3Fired == false then
+            NgocHoi.Bullet3Fired = true
+            NgocHoi:shake(0.08, 1)
+            NgocHoi.bullet_animation:gotoFrame(1)
+            NgocHoi.bullet_animation:resume()
+            NgocHoi.bulletX = 183
+            NgocHoi.bulletY = 180
+        end
+    end
+
+    if NgocHoi.BulletTimer >= NgocHoi.Time_missed_bullet then
+        if NgocHoi.missed_bullet_fired == false then
+            NgocHoi.missed_bullet_fired = true
+            NgocHoi.missed_bullet_animation:gotoFrame(1)
+            NgocHoi.missed_bullet_animation:resume()
+        end
     end
 
     return LevelEnum.Nothing
@@ -154,7 +213,15 @@ function NgocHoi:draw(windowWidth, windowHeight)
         love.graphics.draw(NgocHoi.straw_straight, 65, 178 + NgocHoi.strawOffsets[1])
         love.graphics.draw(NgocHoi.straw_straight, 32, 180 + NgocHoi.strawOffsets[2])
         love.graphics.draw(NgocHoi.straw_straight, -2, 182 + NgocHoi.strawOffsets[3])
-        
+
+        if NgocHoi.bullet_animation.status ~= "paused" then
+            NgocHoi.bullet_animation:draw(NgocHoi.bullet_spritesheet, NgocHoi.bulletX, NgocHoi.bulletY)
+        end
+
+        if NgocHoi.missed_bullet_animation.status ~= "paused" then
+            NgocHoi.missed_bullet_animation:draw(NgocHoi.missed_bullet_spritesheet, NgocHoi.missed_bulletX, NgocHoi.missed_bulletY)
+        end
+
     NgocHoi.cam:detach()
     love.graphics.pop()
 
@@ -163,7 +230,7 @@ function NgocHoi:draw(windowWidth, windowHeight)
     love.graphics.push()
     local fontLoader = FontLoader:getInstance()
     local defaultFont = love.graphics.getFont()
-    local font = fontLoader:loadFont("Texturina", 20)
+    local font = fontLoader:loadFont("Texturina", math.floor(scale * 20))
     love.graphics.origin()
     love.graphics.setColor(0, 0, 0, NgocHoi.TitleAlpha)
     love.graphics.setFont(font)
