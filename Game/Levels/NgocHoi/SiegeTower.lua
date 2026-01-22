5eaf6aba6ffcb926e6e5074f46f9bcb4677e15aa
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-01-22 22:28:13 +0700

    Add siege tower animation assets and stateful logic
    Introduced new image assets for siege tower front, back, and crew animations. Updated SiegeTower.lua to use StatefulObject for managing animated states of the tower and its crew, enabling future animation support.

--- Game/Levels/NgocHoi/SiegeTower.lua
+++ Game/Levels/NgocHoi/SiegeTower.lua
@@ -1,6 +1,8 @@
 local SiegeTower = {}
 SiegeTower.__index = SiegeTower
 
+local StatefulObject = require("Game.Components.StatefulObject")
+
 function SiegeTower.new(world, x, y)
     local self = setmetatable({}, SiegeTower)
 
@@ -10,6 +12,29 @@ function SiegeTower.new(world, x, y)
 
     self.collider = self.world:newCollider("Rectangle", { x + self.sprite1:getWidth() / 2, y + self.sprite1:getHeight() / 2, self.sprite1:getWidth(), self.sprite1:getHeight() })
     self.collider:setType("static")
+
+    self.frontStatefulObject = StatefulObject:new()
+    self.frontSprite1 = love.graphics.newImage("Resources/Images/siege_tower_front1.png")
+    self.frontStatefulObject:addState("sprite", self.frontSprite1)
+    self.frontSprite2 = love.graphics.newImage("Resources/Images/siege_tower_front2.png")
+    self.frontStatefulObject:addState("sprite", self.frontSprite2)
+    self.frontStatefulObject:setState(1)
+
+    self.backStatefulObject = StatefulObject:new()
+    self.backSprite1 = love.graphics.newImage("Resources/Images/siege_tower_back1.png")
+    self.backStatefulObject:addState("sprite", self.backSprite1)
+    self.backSprite2 = love.graphics.newImage("Resources/Images/siege_tower_back2.png")
+    self.backStatefulObject:addState("sprite", self.backSprite2)
+    self.backStatefulObject:setState(1)
+
+    self.guy1StatefulObject = StatefulObject:new()
+    self.guy1Sprite1 = love.graphics.newImage("Resources/Images/siege_tower_guy1_1.png")
+    self.guy1StatefulObject:addState("sprite", self.guy1Sprite1)
+    self.guy1Sprite2 = love.graphics.newImage("Resources/Images/siege_tower_guy1_2.png")
+    self.guy1StatefulObject:addState("sprite", self.guy1Sprite2)
+    self.guy1StatefulObject:setState(1)
+
+
 
     return self
 end
