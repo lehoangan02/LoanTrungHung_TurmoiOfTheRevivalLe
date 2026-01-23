e6b9edd26f7ba950fa8dd0810529576362fa8b93
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-01-23 16:21:03 +0700

    Refactor StatefulObject and update SiegeTower usage
    Refactored StatefulObject to add addSprite and addAnimation methods, and implemented update and draw methods for state handling. Updated SiegeTower to use the new StatefulObject API, added wheel drawing and rotation logic, and fixed constructor naming. Also adjusted NgocHoi background color clearing.

--- Game/Components/StatefulObject.lua
+++ Game/Components/StatefulObject.lua
@@ -7,12 +7,20 @@ function StatefulObject:new(initialState)
     obj.current_state_index = nil
     return obj
 end
-function StatefulObject:addState(stateType, stateValue)
-    local state = {
-        type = stateType, 
-        value = stateValue
-    }
-    table.insert(self.states, state)
+
+function StatefulObject:addSprite(image)
+    table.insert(self.states, {
+        type = "sprite",
+        image = image
+    })
+end
+
+function StatefulObject:addAnimation(animation, spritesheet)
+    table.insert(self.states, {
+        type = "animation",
+        animation = animation,
+        spritesheet = spritesheet
+    })
 end
 
 function StatefulObject:setState(index)
@@ -20,4 +28,52 @@ function StatefulObject:setState(index)
         error("Invalid state index: " .. tostring(index))
     end
     self.current_state_index = index
+end
+
+function StatefulObject:update(dt)
+    if not self.current_state_index then
+        error("Current state is not set.")
+    end
+    local currentState = self.states[self.current_state_index]
+    if currentState.type == "animation" then
+        currentState.animation:update(dt)
+    end
+end
+
+function StatefulObject:draw(x, y)
+    if not self.current_state_index then
+        error("Current state is not set.")
+    end
+    local currentState = self.states[self.current_state_index]
+    if currentState.type == "sprite" then
+        love.graphics.draw(currentState.image, x, y)
+    elseif currentState.type == "animation" then
+        currentState.animation:draw(currentState.spritesheet, x, y)
+    else
+        error("Unknown state type: " .. tostring(currentState.type))
+    end
+end
+
+function StatefulObject:getCurrentAnimation()
+    if not self.current_state_index then
+        error("Current state is not set.")
+    end
+    local currentState = self.states[self.current_state_index]
+    if currentState.type == "animation" then
+        return currentState.animation
+    else
+        return nil
+    end
+end
+
+function StatefulObject:getCurrentSprite()
+    if not self.current_state_index then
+        error("Current state is not set.")
+    end
+    local currentState = self.states[self.current_state_index]
+    if currentState.type == "sprite" then
+        return currentState.image
+    else
+        return nil
+    end
 end
--- Game/Levels/NgocHoi/SiegeTower.lua
+++ Game/Levels/NgocHoi/SiegeTower.lua
@@ -3,8 +3,11 @@ SiegeTower.__index = SiegeTower
 
 local StatefulObject = require("Game.Components.StatefulObject")
 
-function SiegeTower.new(world, x, y)
+function SiegeTower:new(world, x, y)
     local self = setmetatable({}, SiegeTower)
+
+    self.siege_tower_positionX = x
+    self.siege_tower_positionY = y
 
     self.world = world
     self.sprite1 = love.graphics.newImage("Resources/Images/siege_tower.png")
@@ -15,33 +18,60 @@ function SiegeTower.new(world, x, y)
 
     self.frontStatefulObject = StatefulObject:new()
     self.frontSprite1 = love.graphics.newImage("Resources/Images/siege_tower_front1.png")
-    self.frontStatefulObject:addState("sprite", self.frontSprite1)
+    self.frontStatefulObject:addSprite(self.frontSprite1)
     self.frontSprite2 = love.graphics.newImage("Resources/Images/siege_tower_front2.png")
-    self.frontStatefulObject:addState("sprite", self.frontSprite2)
+    self.frontStatefulObject:addSprite(self.frontSprite2)
     self.frontStatefulObject:setState(1)
 
     self.backStatefulObject = StatefulObject:new()
     self.backSprite1 = love.graphics.newImage("Resources/Images/siege_tower_back1.png")
-    self.backStatefulObject:addState("sprite", self.backSprite1)
+    self.backStatefulObject:addSprite(self.backSprite1)
     self.backSprite2 = love.graphics.newImage("Resources/Images/siege_tower_back2.png")
-    self.backStatefulObject:addState("sprite", self.backSprite2)
+    self.backStatefulObject:addSprite(self.backSprite2)
     self.backStatefulObject:setState(1)
 
     self.guy1StatefulObject = StatefulObject:new()
     self.guy1Sprite1 = love.graphics.newImage("Resources/Images/siege_tower_guy1_1.png")
-    self.guy1StatefulObject:addState("sprite", self.guy1Sprite1)
+    self.guy1StatefulObject:addSSprite(self.guy1Sprite1)
     self.guy1Sprite2 = love.graphics.newImage("Resources/Images/siege_tower_guy1_2.png")
-    self.guy1StatefulObject:addState("sprite", self.guy1Sprite2)
+    self.guy1StatefulObject:addSSprite(self.guy1Sprite2)
     self.guy1StatefulObject:setState(1)
 
     self.guy2StatefulObject = StatefulObject:new()
     self.guy2Sprite1 = love.graphics.newImage("Resources/Images/siege_tower_guy2_1.png")
-    self.guy2StatefulObject:addState("sprite", self.guy2Sprite1)
+    self.guy2StatefulObject:addSSprite(self.guy2Sprite1)
     self.guy2Sprite2 = love.graphics.newImage("Resources/Images/siege_tower_guy2_2.png")
-    self.guy2StatefulObject:addState("sprite", self.guy2Sprite2)
+    self.guy2StatefulObject:addSSprite(self.guy2Sprite2)
     self.guy2StatefulObject:setState(1)
 
     self.guy34Sprite = love.graphics.newImage("Resources/Images/siege_tower_guy34.png")
 
+    self.wheel = love.graphics.newImage("Resources/Images/wheel.png")
+    self.wheel:setFilter("nearest", "nearest")
+    self.wheelRotation = 0
+
+
+
     return self
+end
+
+function SiegeTower:update(dt)
+    local rotationSpeed = 120
+    self.wheelRotation = self.wheelRotation + rotationSpeed * dt
+
+end
+
+function SiegeTower:draw()
+    local wheelWidth = self.wheel:getWidth()
+    local wheelHeight = self.wheel:getHeight()
+
+    self.backStatefulObject:draw(self.siege_tower_positionX, self.siege_tower_positionY)
+    love.graphics.draw(self.wheel, self.siege_tower_positionX + 20 + wheelWidth/2, self.siege_tower_positionY + 135 + 3 + wheelHeight/2, math.rad(30 + self.wheelRotation), 1, 1, wheelWidth / 2, wheelHeight / 2)
+    love.graphics.draw(self.wheel, self.siege_tower_positionX + 60 + wheelWidth/2, self.siege_tower_positionY + 135 + 3 + wheelHeight/2, math.rad(45 + self.wheelRotation), 1, 1, wheelWidth / 2, wheelHeight / 2)
+    self.frontStatefulObject:draw(self.siege_tower_positionX, self.siege_tower_positionY)
+    love.graphics.draw(self.wheel, self.siege_tower_positionX + 10 + wheelWidth/2, self.siege_tower_positionY + 135 + 5 + wheelHeight/2, math.rad(60 + self.wheelRotation), 1, 1, wheelWidth / 2, wheelHeight / 2)
+    love.graphics.draw(self.wheel, self.siege_tower_positionX + 50 + wheelWidth/2, self.siege_tower_positionY + 135 + 5 + wheelHeight/2, math.rad(0 + self.wheelRotation), 1, 1, wheelWidth / 2, wheelHeight / 2)
+
+    love.graphics.draw(self.straw, 100, self.siege_tower_positionY - 2)
+    love.graphics.draw(self.straw, 140, self.siege_tower_positionY - 2)
 end
