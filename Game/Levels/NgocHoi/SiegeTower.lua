6d8df66786f7d25319c4929885c7ed5fb0682e11
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-01-23 16:28:15 +0700

    Update SiegeTower.lua

--- Game/Levels/NgocHoi/SiegeTower.lua
+++ Game/Levels/NgocHoi/SiegeTower.lua
@@ -2,6 +2,7 @@ local SiegeTower = {}
 SiegeTower.__index = SiegeTower
 
 local StatefulObject = require("Game.Components.StatefulObject")
+local anim8 = require("Libraries.anim8.anim8")
 
 function SiegeTower:new(world, x, y)
     local self = setmetatable({}, SiegeTower)
@@ -50,7 +51,35 @@ function SiegeTower:new(world, x, y)
     self.wheel:setFilter("nearest", "nearest")
     self.wheelRotation = 0
 
+    self.cannon2_spritesheet = love.graphics.newImage("Resources/Images/LightCannon2.png")
+    self.cannon2_spritesheet:setFilter("nearest", "nearest")
+    self.cannon2_grid = anim8.newGrid(70, 40, self.cannon2_spritesheet:getWidth(), self.cannon2_spritesheet:getHeight())
+    self.cannon2_animation = anim8.newAnimation(self.cannon2_grid('1-6', 1), 0.15, 'pauseAtEnd')
+    self.cannon2_statefulObject = StatefulObject:new()
+    self.cannon2_statefulObject:addAnimation(self.cannon2_animation, self.cannon2_spritesheet)
+    self.cannon2_statefulObject:setState(1)
+    self.TimeCannon2 = 10.5
+    self.cannon2Fired = false
 
+    self.cannon3_spritesheet = love.graphics.newImage("Resources/Images/LightCannon2.png")
+    self.cannon3_spritesheet:setFilter("nearest", "nearest")
+    self.cannon3_grid = anim8.newGrid(70, 40, self.cannon3_spritesheet:getWidth(), self.cannon3_spritesheet:getHeight())
+    self.cannon3_animation = anim8.newAnimation(self.cannon3_grid('1-6', 1), 0.15, 'pauseAtEnd')
+    self.cannon3_statefulObject = StatefulObject:new()
+    self.cannon3_statefulObject:addAnimation(self.cannon3_animation, self.cannon3_spritesheet)
+    self.cannon3_statefulObject:setState(1)
+    self.TimeCannon3 = 11.5
+    self.cannon3Fired = false
+
+    self.cannon4_spritesheet = love.graphics.newImage("Resources/Images/LightCannon2.png")
+    self.cannon4_spritesheet:setFilter("nearest", "nearest")
+    self.cannon4_grid = anim8.newGrid(70, 40, self.cannon4_spritesheet:getWidth(), self.cannon4_spritesheet:getHeight())
+    self.cannon4_animation = anim8.newAnimation(self.cannon4_grid('1-6', 1), 0.15, 'pauseAtEnd')
+    self.cannon4_statefulObject = StatefulObject:new()
+    self.cannon4_statefulObject:addAnimation(self.cannon4_animation, self.cannon4_spritesheet)
+    self.cannon4_statefulObject:setState(1)
+    self.TimeCannon4 = 12.0
+    self.cannon4Fired = false
 
     return self
 end
@@ -66,6 +95,9 @@ function SiegeTower:draw()
     local wheelHeight = self.wheel:getHeight()
 
     self.backStatefulObject:draw(self.siege_tower_positionX, self.siege_tower_positionY)
+    self.cannon2_statefulObject:draw(self.siege_tower_positionX + 40, self.siege_tower_positionY + -2)
+    self.cannon3_statefulObject:draw(self.siege_tower_positionX + 44, self.siege_tower_positionY + 51)
+    self.cannon4_statefulObject:draw(self.siege_tower_positionX + 41, self.siege_tower_positionY + 77)
     love.graphics.draw(self.wheel, self.siege_tower_positionX + 20 + wheelWidth/2, self.siege_tower_positionY + 135 + 3 + wheelHeight/2, math.rad(30 + self.wheelRotation), 1, 1, wheelWidth / 2, wheelHeight / 2)
     love.graphics.draw(self.wheel, self.siege_tower_positionX + 60 + wheelWidth/2, self.siege_tower_positionY + 135 + 3 + wheelHeight/2, math.rad(45 + self.wheelRotation), 1, 1, wheelWidth / 2, wheelHeight / 2)
     self.frontStatefulObject:draw(self.siege_tower_positionX, self.siege_tower_positionY)
