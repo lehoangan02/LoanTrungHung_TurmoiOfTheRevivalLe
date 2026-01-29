bd0b0df94af8d525edb21cf0de0d194fbc3cf4e9
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-01-29 16:16:00 +0700

    Add animated corners and bars to DialogCloud
    Introduces animated top left and bottom right corners, as well as animated top and right bars to the DialogCloud component. Adds support for a new top right corner image and updates the animation logic to sequentially reveal corners and bars. Updates CloudCorners.aseprite and adds CloudTopRightCorner.png.

--- Game/Components/DialogCloud.lua
+++ Game/Components/DialogCloud.lua
@@ -35,19 +35,38 @@ function DialogCloud.new(text, x, y, width, height)
     instance.leftBarCurrentY = instance.leftBarBottomY
 
     instance.bottomBarLeftX = x + 5
-    instance.bottomBarRightX = x + width
+    instance.bottomBarRightX = x + width - 3
     instance.bottomBarAnimationSpeed = math.abs(instance.bottomBarRightX - instance.bottomBarLeftX) / instance.animationTime
     instance.bottomBarCurrentX = instance.bottomBarLeftX
     instance.bottomBarY = y + height - 1
 
+    instance.mainSquareRightX = x + width
+    instance.mainSquareXAnimationSpeed = math.abs(instance.mainSquareRightX - instance.leftBarX) / instance.animationTime
+    instance.mainSquareCurrentX = instance.bottomBarLeftX
+
+
     instance.topLeftCornerSpriteSheet = love.graphics.newImage("Resources/Images/CloudTopLeftCorner.png")
     instance.topLeftCornerSpriteSheet:setFilter("nearest", "nearest")
     instance.topLeftCornerGrid = anim8.newGrid(3, 3, instance.topLeftCornerSpriteSheet:getWidth(), instance.topLeftCornerSpriteSheet:getHeight())
-    instance.topLeftCornerAnimation = anim8.newAnimation(instance.topLeftCornerGrid('1-3', 1), 0.2, 'pauseAtEnd')
+    instance.topLeftCornerAnimation = anim8.newAnimation(instance.topLeftCornerGrid('1-3', 1), 0.1, 'pauseAtEnd')
     instance.topLeftCornerAnimation:gotoFrame(1)
     instance.topLeftCornerAnimation:pause()
     instance.drawTopLeft = false
 
+    instance.bottomRightCornerSpriteSheet = love.graphics.newImage("Resources/Images/CloudBottomRightCorner.png")
+    instance.bottomRightCornerSpriteSheet:setFilter("nearest", "nearest")
+    instance.bottomRightCornerGrid = anim8.newGrid(3, 3, instance.bottomRightCornerSpriteSheet:getWidth(), instance.bottomRightCornerSpriteSheet:getHeight())
+    instance.bottomRightCornerAnimation = anim8.newAnimation(instance.bottomRightCornerGrid('1-3', 1), 0.1, 'pauseAtEnd')
+    instance.bottomRightCornerAnimation:gotoFrame(1)
+    instance.bottomRightCornerAnimation:pause()
+    instance.drawBottomRight = false
+
+    instance.drawTopBar = false
+    instance.drawRightBar = false
+
+    instance.topRightCorner = love.graphics.newImage("Resources/Images/CloudTopRightCorner.png")
+    instance.topRightCorner:setFilter("nearest", "nearest")
+    instance.drawTopRight = false
 
     return instance
 end
@@ -57,19 +76,43 @@ function DialogCloud:update(dt)
         return
     end
     self.sproutAnimation:update(dt)
+    self.topLeftCornerAnimation:update(dt)
+    self.bottomRightCornerAnimation:update(dt)
     if self.leftBarCurrentY > self.leftBarTopY and self.sproutAnimation.status == "paused" then
         self.leftBarCurrentY = self.leftBarCurrentY - self.leftBarAnimationSpeed * dt
         if self.leftBarCurrentY < self.leftBarTopY then
             self.leftBarCurrentY = self.leftBarTopY
             self.drawTopLeft = true
+            self.topLeftCornerAnimation:gotoFrame(1)
+            self.topLeftCornerAnimation:resume()
         end
     end
     if self.bottomBarCurrentX < self.bottomBarRightX and self.sproutAnimation.status == "paused" then
         self.bottomBarCurrentX = self.bottomBarCurrentX + self.bottomBarAnimationSpeed * dt
         if self.bottomBarCurrentX > self.bottomBarRightX then
             self.bottomBarCurrentX = self.bottomBarRightX
+            self.drawBottomRight = true
+            self.bottomRightCornerAnimation:gotoFrame(1)
+            self.bottomRightCornerAnimation:resume()
         end
     end
+
+    if self.topLeftCornerAnimation.status == "paused"
+    and self.topLeftCornerAnimation.position == #self.topLeftCornerAnimation.frames
+    then
+        self.drawTopBar = true
+    end
+
+    if self.bottomRightCornerAnimation.status == "paused"
+    and self.bottomRightCornerAnimation.position == #self.bottomRightCornerAnimation.frames
+    then
+        self.drawRightBar = true
+    end
+
+    if self.drawTopBar and self.drawRightBar then
+        self.drawTopRight = true
+    end
+
 end
 
 function DialogCloud:draw()
@@ -81,13 +124,31 @@ function DialogCloud:draw()
     love.graphics.line(self.leftBarX + 0.5, self.leftBarBottomY, self.leftBarX + 0.5, math.floor(self.leftBarCurrentY))
     --draw bottom bar
     love.graphics.line(self.bottomBarLeftX, self.bottomBarY + 0.5, math.floor(self.bottomBarCurrentX), self.bottomBarY + 0.5)
-
+    --draw top left corner
+    if self.drawTopLeft then
+        self.topLeftCornerAnimation:draw(self.topLeftCornerSpriteSheet, self.x, self.y)
+    end
+    --draw bottom right corner
+    if self.drawBottomRight then
+        self.bottomRightCornerAnimation:draw(self.bottomRightCornerSpriteSheet, self.x + self.width - 3, self.y + self.height - 3)
+    end
+    if self.drawTopBar then
+        love.graphics.setColor(1, 0, 0, 1)
+        love.graphics.line(self.x + 3, self.y + 0.5, self.x + self.width - 3, self.y + 0.5)
+    end
+    if self.drawRightBar then
+        love.graphics.setColor(1, 0, 0, 1)
+        love.graphics.line(self.x + self.width - 0.5, self.y + 3, self.x + self.width - 0.5, self.y + self.height - 3)
+    end
+    if self.drawTopRight then
+        love.graphics.draw(self.topRightCorner, self.x + self.width - 3, self.y)
+    end
+    
     love.graphics.setColor(1, 1, 1, 1)
 
     --set blue
     love.graphics.setColor(0, 0, 1.0, 0.5)
-    -- draw a pixel at (self.x, self.y)
-    -- love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+    love.graphics.rectangle("fill", self.x + 1, self.leftBarCurrentY, self.bottomBarCurrentX - self.bottomBarLeftX, self.leftBarBottomY - self.leftBarCurrentY)
     love.graphics.setColor(1, 1, 1, 1)
 
 end
