4a9dc5c207bd46584982cbe566fe7707696c6f88
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-02-06 16:25:43 +0700

    Update Hinge.lua

--- Game/Levels/BallDrop/Hinge.lua
+++ Game/Levels/BallDrop/Hinge.lua
@@ -1,5 +1,18 @@
-local HInge = {}
+local Hinge = {}
+Hinge.__index = Hinge
+
+machines = {}
+
+function Hinge.new(world, gameMap, layerName)
+    if (gameMap.layers[layerName]) then
+        for _, obj in ipairs(gameMap.layers[layerName].objects) do
+            if obj.properties.machine_id then
+                local id = obj.properties.machine_id
+                machines[id] = machines[id] or {parts = {}}
+            end
+        end
+    end
+end
 
 
-
-return HInge
+return Hinge
