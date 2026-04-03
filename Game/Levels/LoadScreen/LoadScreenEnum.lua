3b14bb821173394e0365eb9087cf3add81f56669
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-04-03 09:17:42 +0700

    Some loading images

---
+++ Game/Levels/LoadScreen/LoadScreenEnum.lua
@@ -0,0 +1,9 @@
+local LoadScreenEnum = {
+    HoangHo = "Resources/Images/Hoangho_gt.jpg",
+    BachHo = "Resources/Images/White_tiger_Hang_Trong.jpg",
+    GaDaiCat = "Resources/Images/Dong_Ho_painting_-_Dai_cat.jpg",
+    LyNguVongNguyen = "Resources/Images/Dong_Ho_painting_-_Ly_Ngu_vong_nguyet.jpg",
+    RongLeTrungHung = "Resources/Images/RongThoiLeTrungHungSen.jpg"
+}
+
+return LoadScreenEnum
