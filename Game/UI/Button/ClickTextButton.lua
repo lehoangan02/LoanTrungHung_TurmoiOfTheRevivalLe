ecf7389715eb63a7fc0332b2dadb0c45d9c7a930
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-04-13 22:00:00 +0700

    Create click text button

---
+++ Game/UI/Button/ClickTextButton.lua
@@ -0,0 +1,8 @@
+local TextButton = require("Game.UI.Button.TextButton")
+
+local ClickTextButton = setmetatable({}, TextButton)
+
+
+function ClickTextButton.new(x, y, width, height, onComplete, color, text)
+    
+end
